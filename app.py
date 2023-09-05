f8de5cfa365c90516490e1a08bc2e2263e102c99
JohnDunthorne <johndunthorne1@gmail.com>
2023-09-05 14:22:25 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+for item in "python":
+    print(item)
+print("-" * 20)
+# this will show all letters in the word
+
+for item in ["one", "two", "three"]:
+    print(item)
+print("-" * 20)
+# this will list off all words in this list
+
+for item in range(5):
+    print(item)
+print("-" * 20)
+# this will list off all numbers from 0-4
+
+for item in range(3, 6):
+    print(item)
+print("-" * 20)
+# this will list off numbers from 3-5
+
+for item in range(2, 20, 2):
+    print(item)
+print("-" * 20)
+# this will list every other number (as per the third 2 in the range) from 2-20
